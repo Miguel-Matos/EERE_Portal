b05ed57e91a7deca59c0cf75eedcb0c380203d8c
Edward Salazar <edwardsalazar@edds-mbp.lan>
2024-06-06 18:45:53 -0500

    Added login button to nav and change width of heroImage lwc

---
+++ outputDir/lwc/heroImage/heroImage.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import EERE_hero from '@salesforce/resourceUrl/EERE_hero';
+
+
+export default class NavMenu extends LightningElement {
+
+    EERE_heroUrl = EERE_hero;
+
+}
