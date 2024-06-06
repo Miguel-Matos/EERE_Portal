3753ab6b359a307cea2fa7238397220c4d5895c2
Edward Salazar <edwardsalazar@edds-mbp.lan>
2024-06-06 18:19:31 -0500

    Added heroImage lwc

--- force-app/main/default/lwc/heroImage/heroImage.js
+++ force-app/main/default/lwc/heroImage/heroImage.js
@@ -1,11 +1,9 @@
 import { LightningElement } from 'lwc';
-import EERE_Logo from '@salesforce/resourceUrl/EERE_Logo';
 import EERE_hero from '@salesforce/resourceUrl/EERE_hero';
 
 
 export default class NavMenu extends LightningElement {
 
-    EERE_LogoUrl = EERE_Logo;
     EERE_heroUrl = EERE_hero;
 
 }
