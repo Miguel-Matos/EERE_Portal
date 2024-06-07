c072214530d362e80dd6999a4b1482d78d32a53d
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2024-06-06 20:13:05 -0400

    need to fix Apex access bug

--- force-app/main/default/lwc/webToCaseInquiry/webToCaseInquiry.js
+++ force-app/main/default/lwc/webToCaseInquiry/webToCaseInquiry.js
@@ -1,12 +1,41 @@
 import { LightningElement } from 'lwc';
+import createInquiry from '@salesforce/apex/InquiryController.createInquiry';
 
 export default class WebToCaseInquiry extends LightningElement {
+  name;
+  email;
+  phone;
+  subject;
+  description;
+
+
 
   handleChange(e) {
+    const field = e.target.name;
+    const val = e.target.value;
 
+    if (field === 'name') {
+      this.firstName = val;
+    } else if (field === 'email') {
+      this.lastName = val;
+    } else if (field === 'phone') {
+      this.lastName = val;
+    } else if (field === 'subject') {
+      this.lastName = val;
+    } else if (field === 'description') {
+      this.lastName = val;
+    } 
   }
 
   handleSubmit() {
-    
+    const inquiry = {
+      Name: this.name,
+      Email__c: this.email,
+      Phone__c: this.phone,
+      Subject__c: this.subject,
+      Description__c: this.description
+    }
+
+    createInquiry({inquiry}).catch(error => console.log(error.body.message));
   }
 }
