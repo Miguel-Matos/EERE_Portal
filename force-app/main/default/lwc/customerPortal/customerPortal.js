ff8db4fb6ac3edf53fa26642cf2192b4a6a45cbd
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2024-06-13 09:40:23 -0400

    application data connected

--- force-app/main/default/lwc/customerPortal/customerPortal.js
+++ force-app/main/default/lwc/customerPortal/customerPortal.js
@@ -1,9 +1,12 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAccountInfo from '@salesforce/apex/AccountController.getAccountInfo';
+import getApplicationInfo from '@salesforce/apex/ApplicationController.getApplicationInfo';
 
 export default class CustomerPortal extends LightningElement {
   @track account;
   @track error;
+  @track application;
+  @track appError
 
   @wire(getAccountInfo)
   wiredAccount({ error, data }) {
@@ -14,6 +17,19 @@ export default class CustomerPortal extends LightningElement {
           this.account = undefined;
           this.error = error;
           console.error('Error fetching account information:', error);
+      }
+  }
+
+
+  @wire(getApplicationInfo)
+  wiredApp({ error, data }) {
+      if (data) {
+          this.application = data;
+          this.appError = undefined;
+      } else if (error) {
+          this.application = undefined;
+          this.appError = error;
+          console.error('Error fetching application information:', error);
       }
   }
 }
