2721c5dd8ef64e6bf53f0d7f0a90b268d836838c
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2024-06-13 16:09:08 -0400

    migrated data to live

--- metadataFolder/lwc/customerPortal/customerPortal.js
+++ metadataFolder/lwc/customerPortal/customerPortal.js
@@ -1,9 +1,14 @@
 import { LightningElement, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getAccountInfo from '@salesforce/apex/AccountController.getAccountInfo';
+import getApplicationInfo from '@salesforce/apex/ApplicationController.getApplicationInfo';
+import uploadFile from '@salesforce/apex/UploadController.uploadFile';
 
 export default class CustomerPortal extends LightningElement {
   @track account;
   @track error;
+  @track application;
+  @track appError
 
   @wire(getAccountInfo)
   wiredAccount({ error, data }) {
@@ -16,4 +21,43 @@ export default class CustomerPortal extends LightningElement {
           console.error('Error fetching account information:', error);
       }
   }
+
+
+  @wire(getApplicationInfo)
+  wiredApp({ error, data }) {
+      if (data) {
+          this.application = data;
+          this.appError = undefined;
+      } else if (error) {
+          this.application = undefined;
+          this.appError = error;
+          console.error('Error fetching application information:', error);
+      }
+  }
+
+  handleUploadFinished(event) {
+    const uploadedFiles = event.detail.files;
+
+    uploadedFiles.forEach(file => {
+        uploadFile({ fileName: file.name, contentVersionId: file.documentId })
+            .then(result => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'File uploaded successfully',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error uploading file',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
+    });
+}
 }
